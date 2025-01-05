0445030f82b8c389bf27ffd0a468707f44e02a5b
awersang <awersang@o2.pl>
2025-01-05 20:58:06 +0100

    udpate

--- app.py
+++ app.py
@@ -27,12 +27,12 @@ def index():
     if request.method == "POST":
         task_content = request.form["content"]
         new_task = Todo(content=task_content)
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect("/")
-        except:
-            return "There was an issue adding your task"
+        # try:
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect("/")
+        # except:
+        #     return "There was an issue adding your task"
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template("index.html", tasks=tasks)
