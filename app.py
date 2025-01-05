ff91743fde25658cebc7cceb4b56ebbdeae82bf7
awersang <awersang@o2.pl>
2025-01-05 22:51:17 +0100

    udpate

--- app.py
+++ app.py
@@ -5,7 +5,12 @@ from datetime import datetime
 # whtf???
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = (
+#     "mysql+pymysql://<DB_USER>:<DB_PASSWORD>@/<DB_NAME>?unix_socket=/cloudsql/<INSTANCE_CONNECTION_NAME>"
+# )
 db = SQLAlchemy(app)
 
 
@@ -27,12 +32,12 @@ def index():
     if request.method == "POST":
         task_content = request.form["content"]
         new_task = Todo(content=task_content)
-        # try:
-        db.session.add(new_task)
-        db.session.commit()
-        return redirect("/")
-        # except:
-        #     return "There was an issue adding your task"
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return "There was an issue adding your task"
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template("index.html", tasks=tasks)
